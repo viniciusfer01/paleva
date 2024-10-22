5e1f6d59a37441a1b877e2558acf64844aaf44e1
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-22 18:05:33 -0300

    store controller and views

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,19 @@
+class StoresController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "stores/index"
+  get "stores/new"
+  get "stores/show"
+  get "stores/create"
+  get "stores/edit"
+  get "stores/update"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
