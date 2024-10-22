23b5691f2c352a22e7538d616a60a1a97abe513e
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-22 20:15:48 -0300

    protected parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :cpf])   
   end
