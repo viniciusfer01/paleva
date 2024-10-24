0c2f5a23eb8fff07cb81ff43c81c7301bb5a3263
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-24 00:02:24 -0300

    empty fields system test

--- spec/system/user_register_store_spec.rb
+++ spec/system/user_register_store_spec.rb
@@ -27,4 +27,26 @@ describe "User registers their store" do
     expect(page).to have_content 'Telefone 99999999999'  
     expect(page).to have_content 'Email pasteis@zezin.com'  
   end
+
+  it "but leaves blank fields" do
+    user = User.create!(cpf: '66101052001', name: 'Zezin', last_name: 'do Teclados', 
+    email: 'zezin@teclados.com', password: 'passwordpass')
+
+    login_as user
+    visit root_path
+    click_on 'Registrar Estabelecimento'
+    fill_in "Razão Social",	with: "Zezin Alimentos LTDA" 
+    fill_in "Nome Fantasia",	with: "" 
+    fill_in "CNPJ",	with: "40599424000139" 
+    fill_in "Endereço Completo",	with: "Rua das tulipas, 18" 
+    fill_in "Telefone",	with: "" 
+    fill_in "Email",	with: "pasteis@zezin.com" 
+    fill_in "Horário de Funcionamento", with: "23456M123456"
+    click_on 'Enviar'
+
+    expect(page).to have_content 'Não foi possível registrar seu estabelecimento.'  
+    expect(page).to have_content 'Verifique os erros abaixo:'   
+    expect(page).to have_content 'Nome Fantasia não pode ficar em branco'   
+    expect(page).to have_content 'Phone não pode ficar em branco'   
+  end
 end
