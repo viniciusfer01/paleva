7316b9342f506eaf4ee34a499f51931b040e6dcd
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-25 01:40:28 -0300

    calories no longer are mandatory

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,5 +2,5 @@ class Dish < ApplicationRecord
   belongs_to :store
   has_one_attached :photo
 
-  validates :name, :description, :calories, presence: true
+  validates :name, :description, presence: true
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -38,6 +38,20 @@ RSpec.describe Dish, type: :model do
         
         expect(result).to eq false  
       end
+
+      it "succeeds when calories are empty" do
+        user = User.create!(cpf: '66101052001', name: 'Zezin', last_name: 'do Teclados', 
+                            email: 'zezin@teclados.com', password: 'passwordpass')
+        store = Store.create!(corporate_name: 'Zezin Alimentos LTDA', brand_name: 'Pastéis Zezin', 
+                              cnpj: '40599424000139', address: 'Rua das tulipas, 18', phone: '2345123456', 
+                              email: 'pasteis@zezin.com', schedule: '23456M123456', user: user)
+
+        dish = Dish.new(name: 'Pizza', description: 'Prato Italiano, que serve 3 a 4 pessoas', store: store)
+
+        result = dish.valid?
+        
+        expect(result).to eq true  
+      end
     end
   end
   
