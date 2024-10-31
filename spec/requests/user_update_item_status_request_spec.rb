64eafd730e771708de441c3cfeb1e80891aa8f8d
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-31 10:52:35 -0300

    update status request tests

---
+++ spec/requests/user_update_item_status_request_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "User sets an order status" do
+  it "if is authenticated" do
+    user = User.create!(cpf: '66101052001', name: 'Zezin', last_name: 'do Teclados', 
+    email: 'zezin@teclados.com', password: 'passwordpass')
+
+    store = Store.create!(corporate_name: 'Zezin Alimentos LTDA', brand_name: 'Pastéis Zezin', 
+    cnpj: '40599424000139', address: 'Rua das tulipas, 18', phone: '2345123456', 
+    email: 'pasteis@zezin.com', schedule: '23456M123456', user: user)
+
+    dish = Dish.create!(name: 'Pizza', description: 'Prato Italiano, que serve 3 a 4 pessoas', calories: 1200, store: store, status: 'inactive')
+    Dish.create!(name: 'Feijoada', description: 'Comida típica da cozinha Brasileira, serve 3 a 4 pessoas', calories: 1500, store: store)
+
+    post(inactive_dish_path(dish))
+    expect(response).to redirect_to new_user_session_path  
+  end
+
+  it "if is responsable" do
+    user = User.create!(cpf: '66101052001', name: 'Zezin', last_name: 'do Teclados', 
+    email: 'zezin@teclados.com', password: 'passwordpass')
+
+    user2 = User.create!(cpf: '37968480019', name: 'Rogerin', last_name: 'do Teclados', 
+    email: 'rogerin@teclados.com', password: 'passwordpass')
+
+    store = Store.create!(corporate_name: 'Zezin Alimentos LTDA', brand_name: 'Pastéis Zezin', 
+    cnpj: '40599424000139', address: 'Rua das tulipas, 18', phone: '2345123456', 
+    email: 'pasteis@zezin.com', schedule: '23456M123456', user: user)
+
+    dish = Dish.create!(name: 'Pizza', description: 'Prato Italiano, que serve 3 a 4 pessoas', calories: 1200, store: store, status: 'inactive')
+    Dish.create!(name: 'Feijoada', description: 'Comida típica da cozinha Brasileira, serve 3 a 4 pessoas', calories: 1500, store: store)
+
+    login_as user2
+    post(inactive_dish_path(dish))
+    expect(response).to redirect_to new_store_path  
+  end
+end
