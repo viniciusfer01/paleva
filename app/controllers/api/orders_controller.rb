355059c3a8c3472081acb44f0c0a1031787032f1
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-11-13 19:19:43 -0300

    orders are filtered by status and store

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,5 +1,10 @@
 class Api::OrdersController < ActionController::API
   def index 
+    if params[:status]
+      orders = Store.find_by(code: params[:code]).orders.find_by(status: params[:status])
+      return render status: 200, json: orders 
+    end
+
     orders = Store.find_by(code: params[:code]).orders
     render status: 200, json: orders 
   end
--- spec/requests/apis/order_api_spec.rb
+++ spec/requests/apis/order_api_spec.rb
@@ -9,9 +9,6 @@ describe 'Order API' do
       store = Store.create!(corporate_name: 'Zezin Alimentos LTDA', brand_name: 'Pastéis Zezin', 
                     cnpj: '40599424000139', address: 'Rua das tulipas, 18', phone: '2345123456', 
                     email: 'pasteis@zezin.com', schedule: '23456M123456', user: user)
-
-      store.menus.create!(name: 'Almoço')                  
-      store.menus.create!(name: 'Jantar')   
       
       dish = Dish.create!(name: 'Feijoada', description: 'Feijoada completa', store: store)
       beverage = Beverage.create!(name: 'Coca-Cola', description: 'Refrigerante de cola',
@@ -31,6 +28,50 @@ describe 'Order API' do
       expect(response.status).to eq 200  
       expect(response.content_type).to include 'application/json'  
       expect(response.body).to include(order.code)  
+    end
+
+    it 'filters orders by status' do 
+      first_user = User.create!(cpf: '96287760028', name: 'Geraldo', last_name: 'Antunes', 
+      email: 'geraldo@refrigeracao.com', password: 'passwordpass')
+      
+      second_user = User.create!(cpf: '66101052001', name: 'Zezin', last_name: 'do Teclados', 
+                        email: 'zezin@teclados.com', password: 'passwordpass')
+
+      Store.create!(corporate_name: 'Zezin Alimentos2 LTDA', brand_name: 'Pastéis Zezin2', 
+      cnpj: '66345931000130', address: 'Rua das tulipas, 180', phone: '2345123426', 
+      email: 'pasteis2@zezin.com', schedule: '23456M12346', user: first_user)
+
+      store = Store.create!(corporate_name: 'Zezin Alimentos LTDA', brand_name: 'Pastéis Zezin', 
+                    cnpj: '40599424000139', address: 'Rua das tulipas, 18', phone: '2345123456', 
+                    email: 'pasteis@zezin.com', schedule: '23456M123456', user: second_user)
+
+      
+      dish = Dish.create!(name: 'Feijoada', description: 'Feijoada completa', store: store)
+      beverage = Beverage.create!(name: 'Coca-Cola', description: 'Refrigerante de cola',
+                                  is_alcoholic: false, store: store)
+
+      first_portion = dish.portions.create!(description: 'Porção grande', price: 3000, dish: dish)
+      second_portion = beverage.portions.create!(description: 'Lata 350ml', price: 500, beverage: beverage)
+
+      order = Order.create!(name: second_user.name, phone: '99999999999', email: second_user.email,
+      cpf: second_user.cpf, price: 3500, user: second_user, store: store)
+      
+      second_order = Order.create!(name: second_user.name, phone: '99999999999', email: second_user.email,
+      cpf: second_user.cpf, price: 9000, user: second_user, store: store, status: 'ready')
+      
+      other_order = Order.create!(name: first_user.name, phone: '99999999999', email: first_user.email,
+                            cpf: first_user.cpf, price: 3500, user: first_user, store: store)
+
+      order.order_items.create!(order: order, portion: first_portion, note: 'Bem quente')
+      order.order_items.create!(order: order, portion: second_portion, note: 'Bem gelada')
+
+      get "/api/orders?code=#{store.code}&status=ready"
+
+      expect(response.status).to eq 200  
+      expect(response.content_type).to include 'application/json'  
+      expect(response.body).not_to include(order.code)  
+      expect(response.body).not_to include(other_order.code)  
+      expect(response.body).to include(second_order.code)  
     end
   end
 end
